4f7df4d49b6703272cf0fbaa607fa0dcb08cf2da
Isabella Vanderlinde Berkembrock <130788816+isabella1709@users.noreply.github.com>
2025-04-10 12:14:16 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({ // define as credenciais de conexão com o banco de dados MYSQL
-    host: "localhost",
-    user: "root",
-    password: "Isa@1234",
-    database: "crud"
+    host: "",
+    user: "",
+    password: "",
+    database: ""
 });
